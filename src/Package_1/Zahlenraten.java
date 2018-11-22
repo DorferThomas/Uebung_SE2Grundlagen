b8b09ab18fe0fcfea8e434224dc37a3d57829580
Dorfer Thomas Josef <1810653831@stud.fh-kufstein.ac.at>
2018-11-22 14:32:27 +0100

    Aufgabe3BenutzerVersuchtDieZusfallszahlZuEraten

--- src/Package_1/Zahlenraten.java
+++ src/Package_1/Zahlenraten.java
@@ -1,5 +1,6 @@
 package Package_1;
 
+import javax.swing.*;
 import java.util.Random;
 
 public class Zahlenraten {
@@ -15,16 +16,29 @@ public class Zahlenraten {
         int zufallszahl = rnd.nextInt(max - min + 1) + min;
 
         // hier wird mit dem % geprüft ob die zufällig berechnete Zahl gerade oder ungerade ist.
-        if (zufallszahl%2==0)
+        if (zufallszahl%2==0) // Wenn die Zahl gerade ist wird das unten stehente in die Konsole ausgegeben.
         {
-            System.out.println("Die Zahl " + zufallszahl + " ist gerade.");
-        }else
+            System.out.println("Die Zahl " +
+                    "ist gerade.");
+        }else //falls die Zahl nicht gerade ist wird das unten stehente in die Konsole ausgegeben.
         {
-            System.out.println("Die Zahl " + zufallszahl + " ist nicht gerade.");
+            System.out.println("Die Zahl ist nicht gerade.");
         }
 
+        //es wird vom Benutzer verlangt eine Zahl von 1-10 einzugeben
+        int eingabe = Integer.parseInt(JOptionPane.showInputDialog("Geben Sie eine Ganzzahl von 1-10 ein."));
+
+        //die eingegebene Zahl wird nun mit der Zufallszahl verglichen.
+        if (eingabe==zufallszahl)
+        {
+            System.out.println("Glückwunsch sie haben die gleiche Zahl wie der Zufallsgenerator getroffen. Trotzdem bekommen sie keinen Preis. Die Zahl war: " + zufallszahl);
+        }else
+        {
+            System.out.println("Leider nicht Gewonnen°! Die Zahl war: " + zufallszahl);
+        }
 
 
 
     }
 }
+
