2e6ce253fcb384db89f4440feb365a8735436e17
Dorfer Thomas Josef <1810653831@stud.fh-kufstein.ac.at>
2018-11-22 15:56:12 +0100

    Aufgabe5MittelsWhileSchleifeEingabeMitZufallszahlVergleichenUndMax3DurchgängeErlauben

--- src/Package_1/Zahlenraten.java
+++ src/Package_1/Zahlenraten.java
@@ -26,23 +26,33 @@ public class Zahlenraten {
         {
             System.out.println("Die Zahl ist nicht gerade.");
         }
-        System.out.println(zufallszahl);
-                                                                                                                         //es wird vom Benutzer verlangt eine Zahl von 1-10 einzugeben
-                                                                                                                         // int eingabe = Integer.parseInt(JOptionPane.showInputDialog("Geben Sie eine Ganzzahl von 1-10 ein."));
-        //mit den FOR Schleifenkopf() gibt man an wie oft die Schleife durchlaufen soll. In diesem Fall von 0-2 also 3 mal.
-        //in der For Schleife wird dann geprüft, ob die eingegebene Zahl mit der Zufallszahl übereinstimmt.
-        for (int i=0;i<3;++i ){
+        System.out.println(zufallszahl);                                                                                //es wird vom Benutzer verlangt eine Zahl von 1-10 einzugeben
 
+
+
+        int versuche = 3;                                                                                               //Aufgabe 5: Mit der While Schleife werden gleich wie bei der FOR schleife die 3 Durchgänge bestimmt.
+                while (versuche!=0)                                                                                     //Die Schleife wird so lange durchgelaufen, bis die Durchgänge auf 0 sind. also max. 3 mal
+        {
             int eingabe = Integer.parseInt(JOptionPane.showInputDialog("Geben Sie eine Ganzzahl von 1-10 ein."));
             if (eingabe==zufallszahl)
             {
-                System.out.println("Glückwunsch sie haben die Zahl erraten.");
+                System.out.println("Glückwunsch sie haben die Zahl erraten.");                                          //Wird die Zahl auf das erste Mal erraten, bricht alles ab und diese sout. Meldung wird ausgegeben.
                 break;
             }else
             {
-                System.out.println("Leider nicht gewonnen, versuchen sie es nochmal!");
+                if (eingabe>=10)                                                                                        //Mit der IF Schleife wird nochmal geprüft, ob die eingegebene Zahl im richtigen Wertebereich ist.
+                {                                                                                                       //Falls das nicht der Fall ist, wird die sout. ausgegeben und es wird als kein Versuch gerechnet und beginnt von vorne.
+                    System.out.println("Sie haben eine zu hohe Zahl eingegeben.");
+                }else
+                {
+                    --versuche;
+                    System.out.println("Leider nicht gewonnen, sie haben noch " + versuche + " versuche!");             //hier wird die Anzahl der Versuche noch inkrementiert --1 und danach zusätzlich noch die anzahl der Versuche ausgegeben.
+
+                }
             }
-        };
+        }
+
+
 
 
             /*Dies war Aufgabe 2 - 3 welche durch 4 ersetzt wurde.
@@ -69,6 +79,26 @@ public class Zahlenraten {
                        System.out.println("Leider nicht Gewonnen°! Die Zahl war: " + zufallszahl);
                    }
             }*/
+
+
+
+
+        /*                                                                                                              // int eingabe = Integer.parseInt(JOptionPane.showInputDialog("Geben Sie eine Ganzzahl von 1-10 ein."));
+        //mit den FOR Schleifenkopf() gibt man an wie oft die Schleife durchlaufen soll. In diesem Fall von 0-2 also 3 mal.
+        //in der For Schleife wird dann geprüft, ob die eingegebene Zahl mit der Zufallszahl übereinstimmt.
+        for (int i=0;i<3;++i ){
+
+            int eingabe = Integer.parseInt(JOptionPane.showInputDialog("Geben Sie eine Ganzzahl von 1-10 ein."));
+            if (eingabe==zufallszahl)
+            {
+                System.out.println("Glückwunsch sie haben die Zahl erraten.");
+                break;
+            }else
+            {
+                System.out.println("Leider nicht gewonnen, versuchen sie es nochmal!");
+            }
+        };
+        */
 
 
 
